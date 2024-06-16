c8b6bb991ac82ac9dd0ce7f7cee1806745032a12
AfkunG <anthony.g.rivera.i@gmail.com>
2024-06-16 18:36:36 -0500

    Traslado a Maven

---
+++ src/main/java/Reportes/ReportesT.java
@@ -0,0 +1,218 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reportes;
+
+import Modelo.Conexion;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.sql.SQLException;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author AfkunGT
+ */
+public class ReportesT {
+    Connection con;
+    Conexion cn = new Conexion();
+    PreparedStatement ps;
+    ResultSet rs;
+    
+    //REPORTE CLIENTES
+    public void ReportesClientes() {
+        Document documento = new Document();
+        try {
+            String ruta = System.getProperty("user.home");
+            PdfWriter.getInstance(documento, new FileOutputStream(ruta + "/Desktop/Reporte_Clientes.pdf"));
+            Image header = Image.getInstance("src/Img/reporte.png");
+            header.scaleToFit(650, 1000);
+            header.setAlignment(Chunk.ALIGN_CENTER);
+            //formato al texto
+            Paragraph parrafo = new Paragraph();
+            parrafo.setAlignment(Paragraph.ALIGN_CENTER);
+            parrafo.add("Reporte creado por HyM \n\n");
+            parrafo.setFont(FontFactory.getFont("Tahoma", 18, Font.BOLD, BaseColor.DARK_GRAY));
+            parrafo.add("Reporte de Clientes \n\n");
+
+            documento.open();
+            //agregamos los datos
+            documento.add(header);
+            documento.add(parrafo);
+
+            PdfPTable tabla = new PdfPTable(5);
+            tabla.addCell("Codigo");
+            tabla.addCell("DNI");
+            tabla.addCell("Nombre");
+            tabla.addCell("Telefono");
+            tabla.addCell("Direccion");
+
+            try {
+                Connection con = cn.getConnection();
+                PreparedStatement pst = con.prepareStatement(
+                        "select id, dni, nombre, telefono , direccion from clientes");
+                ResultSet rs = pst.executeQuery();
+                if (rs.next()) {
+                    do {
+                        tabla.addCell(rs.getString(1));
+                        tabla.addCell(rs.getString(2));
+                        tabla.addCell(rs.getString(3));
+                        tabla.addCell(rs.getString(4));
+                        tabla.addCell(rs.getString(5));
+                    } while (rs.next());
+                    documento.add(tabla);
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Error 4 en: " + e);
+            }
+            documento.close();
+            
+            JOptionPane.showMessageDialog(null, "Reporte creado");
+
+        } catch (DocumentException e) {
+            System.out.println("Error 1 en: " + e);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error 2 en: " + ex);
+        } catch (IOException ex) {
+            System.out.println("Error 3 en: " + ex);
+        }
+    }
+    
+    //REPORTE PROVEEDOR
+    public void ReportesProveedor() {
+        Document documento = new Document();
+        try {
+            String ruta = System.getProperty("user.home");
+            PdfWriter.getInstance(documento, new FileOutputStream(ruta + "/Desktop/Reporte_Proveedor.pdf"));
+            Image header = Image.getInstance("src/Img/reporte.png");
+            header.scaleToFit(650, 1000);
+            header.setAlignment(Chunk.ALIGN_CENTER);
+            //formato al texto
+            Paragraph parrafo = new Paragraph();
+            parrafo.setAlignment(Paragraph.ALIGN_CENTER);
+            parrafo.add("Reporte creado por HyM \n\n");
+            parrafo.setFont(FontFactory.getFont("Tahoma", 18, Font.BOLD, BaseColor.DARK_GRAY));
+            parrafo.add("Reporte de Proveedores \n\n");
+
+            documento.open();
+            //agregamos los datos
+            documento.add(header);
+            documento.add(parrafo);
+
+            PdfPTable tabla = new PdfPTable(5);
+            tabla.addCell("Codigo");
+            tabla.addCell("RUC");
+            tabla.addCell("Nombre");
+            tabla.addCell("Telefono");
+            tabla.addCell("Direccion");
+
+            try {
+                Connection con = cn.getConnection();
+                PreparedStatement pst = con.prepareStatement(
+                        "select id, ruc, nombre, telefono , direccion from proveedor");
+                ResultSet rs = pst.executeQuery();
+                if (rs.next()) {
+                    do {
+                        tabla.addCell(rs.getString(1));
+                        tabla.addCell(rs.getString(2));
+                        tabla.addCell(rs.getString(3));
+                        tabla.addCell(rs.getString(4));
+                        tabla.addCell(rs.getString(5));
+                    } while (rs.next());
+                    documento.add(tabla);
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Error 4 en: " + e);
+            }
+            documento.close();
+            
+            JOptionPane.showMessageDialog(null, "Reporte creado");
+
+        } catch (DocumentException e) {
+            System.out.println("Error 1 en: " + e);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error 2 en: " + ex);
+        } catch (IOException ex) {
+            System.out.println("Error 3 en: " + ex);
+        }
+    }
+    
+    //REPORTE PROVEEDOR
+    public void ReportesProducto() {
+        Document documento = new Document();
+        try {
+            String ruta = System.getProperty("user.home");
+            PdfWriter.getInstance(documento, new FileOutputStream(ruta + "/Desktop/Reporte_Productos.pdf"));
+            Image header = Image.getInstance("src/Img/reporte.png");
+            header.scaleToFit(650, 1000);
+            header.setAlignment(Chunk.ALIGN_CENTER);
+            //formato al texto
+            Paragraph parrafo = new Paragraph();
+            parrafo.setAlignment(Paragraph.ALIGN_CENTER);
+            parrafo.add("Reporte creado por HyM \n\n");
+            parrafo.setFont(FontFactory.getFont("Tahoma", 18, Font.BOLD, BaseColor.DARK_GRAY));
+            parrafo.add("Reporte de Productos \n\n");
+
+            documento.open();
+            //agregamos los datos
+            documento.add(header);
+            documento.add(parrafo);
+
+            PdfPTable tabla = new PdfPTable(6);
+            tabla.addCell("ID");
+            tabla.addCell("Codigo");
+            tabla.addCell("Nombre");
+            tabla.addCell("Proveedor");
+            tabla.addCell("Stock");
+            tabla.addCell("Precio");
+            try {
+                Connection con = cn.getConnection();
+                PreparedStatement pst = con.prepareStatement(
+                        "select id, codigo, nombre, proveedor , stock, precio from productos");
+                ResultSet rs = pst.executeQuery();
+                if (rs.next()) {
+                    do {
+                        tabla.addCell(rs.getString(1));
+                        tabla.addCell(rs.getString(2));
+                        tabla.addCell(rs.getString(3));
+                        tabla.addCell(rs.getString(4));
+                        tabla.addCell(rs.getString(5));
+                        tabla.addCell(rs.getString(6));
+                    } while (rs.next());
+                    documento.add(tabla);
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Error 4 en: " + e);
+            }
+            documento.close();
+            
+            JOptionPane.showMessageDialog(null, "Reporte creado");
+
+        } catch (DocumentException e) {
+            System.out.println("Error 1 en: " + e);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error 2 en: " + ex);
+        } catch (IOException ex) {
+            System.out.println("Error 3 en: " + ex);
+        }
+    }
+}
