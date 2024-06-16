90cd502df2fd9a414a9330e0bcb6d80e5af466e6
Marko <Marko@DESKTOP-RS86C8A>
2024-06-16 18:45:39 -0500

    Vuelta a la normalida

--- src/main/java/Modelo/VentaDao.java
+++ src/main/java/Modelo/VentaDao.java
@@ -1,18 +1,18 @@
 
 package Modelo;
 
-//import com.itextpdf.text.BaseColor;
-//import com.itextpdf.text.Chunk;
-//import com.itextpdf.text.Document;
-//import com.itextpdf.text.DocumentException;
-//import com.itextpdf.text.Element;
-//import com.itextpdf.text.Font;
-//import com.itextpdf.text.Image;
-//import com.itextpdf.text.Paragraph;
-//import com.itextpdf.text.Phrase;
-//import com.itextpdf.text.Rectangle;
-//import com.itextpdf.text.pdf.PdfPCell;
-//import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.awt.Desktop;
 import java.io.File;
